64da7050c56abd20fa049d338d8fb72cd993989b
alissonif <josealissonif@gmail.com>
2024-03-03 16:21:07 -0300

    feat: add footer component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -40,6 +40,16 @@ export default async function Home() {
           })}
         </div>
       </div>
+      <div className="mt-6 mb-[4.5rem]">
+        <h2 className="px-5 text-xs uppercase text-gray-400 font-bold mb-3">
+          Populares
+        </h2>
+        <div className="flex px-5 gap-4 overflow-x-auto [&::-webkit-scrollbar]:hidden">
+          {barbershops.map((item) => {
+            return <BarberShopItem key={item.id} barbershop={item} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ app/_components/footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="w-full bg-secondary py-6 px-5">
+      <p className="text-gray-400 text-sm font-bold bg-opacity-75">
+        @ 2023 Copyright FSW Barber
+      </p>
+    </div>
+  );
+};
+
+export default Footer;
